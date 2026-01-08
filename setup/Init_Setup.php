d84e2769439f80f38d497b526784363ea35dad16
ramodesigns <ramodesignsuk@gmail.com>
2026-01-08 22:43:12 +0000

    added init setup class

---
+++ setup/Init_Setup.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WordPress REST API Class for User Preferences
+ *
+ * Registers REST API endpoints:
+ * - postmetadata/v1/preferences/update (protected)
+ * - postmetadata/v1/preferences/retrieve (protected)
+ * - postmetadata/v1/preferences/retrievepublic (public)
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+class Init_Setup
+{
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+
+    }
+
+}
---
+++ setup/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once plugin_dir_path( __FILE__ ) . 'Init_Setup.php';
+new Init_Setup();
+
